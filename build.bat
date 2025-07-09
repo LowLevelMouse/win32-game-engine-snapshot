c9b196152efd37d111c0a09659b3925273feaeaa
Justin Wegner <justin.wegner.dev@gmail.com>
2025-07-09 07:28:32 -0400

    Added missing files and fixed batch file

--- build.bat
+++ build.bat
@@ -3,9 +3,9 @@ setlocal
 
 del /q *.obj *.pdb *.exe *.ilk *.lib *.exp
 
-set CFLAGS=/nologo /Zi /Od /W4 /Fe:win32.exe /DUSE_STRETCHDIBITS
-set SOURCE=win32.cpp
-set LFLAGS=/DEBUG /SUBSYSTEM:WINDOWS user32.lib gdi32.lib ole32.lib xaudio2.lib
+set CFLAGS=/nologo /Zi /Od /W4 /Fe:win32.exe /Iinclude
+set SOURCE=win32.cpp gl.c wgl.c
+set LFLAGS=/DEBUG /SUBSYSTEM:WINDOWS user32.lib gdi32.lib ole32.lib xaudio2.lib opengl32.lib
 
 cl %CFLAGS% %SOURCE% /link %LFLAGS%
 
